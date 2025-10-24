bf02ffc614c9249a1bfd72c399b79b481dcffce7
StevenIwan <steven.iwan070@gmail.com>
2025-10-24 13:26:08 +0700

    SukaSukaGW

---
+++ Menu.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Menu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Menu extends World
+{
+
+    /**
+     * Constructor for objects of class Menu.
+     * 
+     */
+    public Menu()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+    
+    private void ButtonMenu(){
+    BtnTutor Tutorial = new BtnTutor();
+    BtnStart Start = new BtnStart();
+    BtnExit Exit = new BtnExit();
+    
+    addObject(Tutorial, 200, 350); // (actor, x, y)
+    addObject(Start,    300, 350); // (actor, x, y)
+    addObject(Exit,     400, 350); // (actor, x, y)
+    }
+}
