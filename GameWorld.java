332c8e9f9fb1598b63519b814fda8f015440e9bc
Ragiara <gaddielsitorus54@gmail.com>
2025-10-27 21:56:13 +0700

    v3.0
    Boat Animation (Left & Right). Map Resolution Changes to 960x540.

--- GameWorld.java
+++ GameWorld.java
@@ -15,7 +15,7 @@ public class GameWorld extends World {
     private final SimpleTimer fishSpawnTimer = new SimpleTimer();
 
     public GameWorld() {
-        super(648, 468, 1);
+        super(960, 540, 1);
         setPaintOrder(Hook.class, Boat.class); // hook di depan boat (opsional)
         prepare();
         updateHUD();
@@ -26,7 +26,7 @@ public class GameWorld extends World {
         GreenfootImage bg = new GreenfootImage("24.jpg"); 
 
         // 3. Paksa gambar untuk pas dengan ukuran dunia (648x468)
-        bg.scale(648, 468);
+        bg.scale(960, 540);
 
         // 4. Atur gambar yang sudah dikecilkan
         setBackground(bg);
@@ -38,10 +38,10 @@ public class GameWorld extends World {
         int boatY = 120;
 
         boat = new Boat();
-        addObject(boat, boatX, boatY);
+        addObject(boat, boatX, 250);
 
         hook = new Hook(boat);              // hook “terikat” ke boat
-        addObject(hook, boatX, boatY + 60);
+        addObject(hook, boatX, boatY + 180);
 
         startTimer(300);                      // mulai timer 60s (ubah via Level nanti)
     }
