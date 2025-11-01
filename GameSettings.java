fef604638439ec6552af9babde5fa8664fa10618
Steven Iwan <steven.iwan070@gmail.com>
2025-11-01 22:19:20 +0700

    Add Array Health

--- GameSettings.java
+++ GameSettings.java
@@ -8,6 +8,12 @@ public class GameSettings
 {
     public static String difficulty = "Easy";
     
+    public static final int[][] EnemyHealth = {
+        {3, 5, 7}, // Easy (Lvl 1=3, Lvl 2=5, Lvl 3=7)
+        {5, 7, 9}, // Medium (Lvl 1=5, Lvl 2=7, Lvl 3=9)
+        {7, 9, 11}  // Hard (Lvl 1=7, Lvl 2=9, Lvl 3=11)
+    };
+    
     // Ukuran ikan berdasarkan level
     // [level][0] = lebar, [level][1] = tinggi
     public static final int[][] commonFishSize = {
